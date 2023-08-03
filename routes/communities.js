fdf6472f1ffa2b720e4fe1a6825a1a6a21a8a988
Mar-Nb <niombela.martin@gmail.com>
2023-08-03 16:11:44 +0200

    Ajout du feed

--- routes/communities.js
+++ routes/communities.js
@@ -4,86 +4,104 @@ var router = express.Router();
 const User = require("../models/users");
 const Community = require("../models/communities");
 const { checkBody } = require("../modules/checkBody");
+const { default: mongoose } = require("mongoose");
+const Object = require("../models/objects");
 
 /**
  * POST - Join a community
  */
 router.post("/join", async (req, res) => {
-  if (!checkBody(req.body, ["token", "accessCode", "name"])) {
-    res.json({ result: false, error: "Missing or invalid field" });
-    return;
-  }
+	if (!checkBody(req.body, ["token", "accessCode", "name"])) {
+		res.json({ result: false, error: "Missing or invalid field" });
+		return;
+	}
 
-  const { token, accessCode, name } = req.body;
+	const { token, accessCode, name } = req.body;
 
-  const commu = await Community.findOne({ name });
-  const user = await User.findOne({ token });
+	const commu = await Community.findOne({ name });
+	const user = await User.findOne({ token });
 
-  if (user.community.includes(commu._id)) {
-    res.json({ result: false, error: "Community already joined" });
-    return;
-  } else if (commu.accessCode === accessCode) {
-    const updateRes = await User.updateOne(
-      { token },
-      { $push: { community: commu._id } }
-    );
-    res.json({
-      result: updateRes.modifiedCount === 1,
-      localisation: commu.localisation,
-      description: commu.description,
-      name: commu.name,
-    });
-  } else {
-    res.json({ result: false, error: "Wrong access code" });
-  }
+	if (user.community.includes(commu._id)) {
+		res.json({ result: false, error: "Community already joined" });
+		return;
+	} else if (commu.accessCode === accessCode) {
+		const updateRes = await User.updateOne(
+			{ token },
+			{ $push: { community: commu._id } }
+		);
+		res.json({
+			result: updateRes.modifiedCount === 1,
+			localisation: commu.localisation,
+			description: commu.description,
+			name: commu.name,
+		});
+	} else {
+		res.json({ result: false, error: "Wrong access code" });
+	}
 });
 
 const generateRandomAccessCode = (length) => {
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let accessCode = "";
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    accessCode += characters[randomIndex];
-  }
-  return accessCode;
+	const characters =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let accessCode = "";
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		accessCode += characters[randomIndex];
+	}
+	return accessCode;
 };
 
+/**
+ * POST - Création d'une communauté par User
+ */
 router.post("/create", async (req, res) => {
-  const { name, localisation, description, photo, isPrivate } = req.body;
+	const { name, localisation, description, photo, isPrivate } = req.body;
 
-  // Vérifier si il y a une commu avec le même nom dans la base de données
-  const existingCommunity = await Community.findOne({ name });
-  if (existingCommunity) {
-    // Une commu avec le même nom existe déjà
-    return res.json({
-      result: false,
-      error: "Une communauté avec le même nom existe déjà",
-    });
-  }
+	// Vérifier si il y a une commu avec le même nom dans la base de données
+	const existingCommunity = await Community.findOne({ name });
+	if (existingCommunity) {
+		// Une commu avec le même nom existe déjà
+		return res.json({
+			result: false,
+			error: "Une communauté avec le même nom existe déjà",
+		});
+	}
 
-  const accessCode = generateRandomAccessCode(5);
+	const accessCode = generateRandomAccessCode(5);
 
-  // Creation de la nouvelle communauté
-  const newCommunity = await new Community({
-    name,
-    localisation,
-    accessCode,
-    description,
-    photo,
-    creationDate: new Date(),
-    isPrivate,
-  });
-  
-
-  await newCommunity.save();
+	// Creation de la nouvelle communauté
+	const newCommunity = await new Community({
+		name,
+		localisation,
+		accessCode,
+		description,
+		photo,
+		creationDate: new Date(),
+		isPrivate,
+	});
 
 
-  res.json({
-    result: true,
-    accessCode: newCommunity.accessCode,
-    name: newCommunity.name,
-  });
+	await newCommunity.save();
+
+
+	res.json({
+		result: true,
+		accessCode: newCommunity.accessCode,
+		name: newCommunity.name,
+	});
+});
+
+/**
+ * GET - Récupération des objets dispo dans les communautés de User (sauf les siens)
+ */
+router.get('/feed/:token', async (req, res) => {
+	const user = await User.findOne({ token: req.params.token });
+
+	const listCommu = user.community.map(id => new mongoose.Types.ObjectId(id));
+	const ownObjects = await Object.find({ idUser: user._id }, '_id');
+	const objectsfound = await Object.find({ _id: { $nin: ownObjects }, isAvailable: true, availableIn: { $in: listCommu } }).populate('idUser');
+
+	res.json({ result: true, items: objectsfound });
 });
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -2,29 +2,40 @@ const express = require('express');
 const router = express.Router();
 
 const Object = require('../models/objects');
-const User = require("../models/users");
-const Community = require("../models/communities");
+const User = require('../models/users');
+const Community = require('../models/communities');
 const { default: mongoose } = require('mongoose');
 const { checkBody } = require('../modules/checkBody');
 
 /**
  * GET - Route pour rechercher les objets d'une communauté à laquelle le user a adhéré
- * body: "token", "name", "communitiesId"
+ * body: 'token', 'name', 'communitiesId'
  */
 router.post('/', async (req, res) => {
-	if (!checkBody(req.body, ["token", "name"])) {
-		res.json({ result: false, error: "Missing or invalid field" });
+	if (!checkBody(req.body, ['token', 'name'])) {
+		res.json({ result: false, error: 'Missing or invalid field' });
 		return;
 	}
 
 	/* Récupération de base, par communité */
+	const user = await User.findOne({ token: req.body.token });
+
+	// Construction du filtre
+	const objFilter = {
+		isAvailable: true,
+		name: new RegExp(req.body.name.trim(), 'i')
+	};
 
 	// On retire des résultats de la requête les objets du User disponibles dans les communautés qu'il ne veut pas voir
 	// Il faut transformer les strings en mongoose.Types.ObjectId
-	const listCommu = req.body.communitiesId.map(id => new mongoose.Types.ObjectId(id));
+	let listCommu = null;
+	if (req.body.communitiesId) { listCommu = req.body.communitiesId.map(id => new mongoose.Types.ObjectId(id)); }
+	else { listCommu = user.community.map(id => new mongoose.Types.ObjectId(id)); }
+
+	objFilter['availableIn'] = { $in: listCommu };
 
 	// On trouve les objets disponibles dans les communautés du User
-	const objectsfound = await Object.find({ name: new RegExp(req.body.name.trim(), 'i'), isAvailable: true, availableIn: { $in: listCommu } }).populate('idUser');
+	const objectsfound = await Object.find(objFilter).populate('idUser');
 
 	/* Calcul de la distance des objets */
 	// {...}
